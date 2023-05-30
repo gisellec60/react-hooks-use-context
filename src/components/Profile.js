9b3034598e1776b1c17f5fff001cb26df1eec3c5
Giselle <gisellec60@gmail.com>
2023-05-30 15:47:11 -0400

    commit code along

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,22 @@
-import React from "react";
+//import the useContext hook
+import React, {useContext} from "react";
 import Interests from "./Interests";
+//import the UserContext we created
+import { UserContext } from "../context/user";
 
-function Profile({ user, theme }) {
+
+function Profile() {
+  //call the useContext hook with our UserContext
+  const {user} =useContext(UserContext)
+ 
   if (!user) return <h2>Please Login To View Profile</h2>;
   return (
     <div>
       <h2>{user.name}'s Profile</h2>
-      <Interests interests={user.interests} theme={theme} />
+      <Interests interests={user.interests} />
     </div>
   );
 }
 
 export default Profile;
+ 
